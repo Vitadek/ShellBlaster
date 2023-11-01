66506cc68e0341511c2cda4f4cab8e0c49b2f932
Diego Uy <diego.uy@encryptedgardens.com>
2023-11-01 10:54:58 -1000

    I need to add more functions, but menu works now#

--- ui/src/main.cpp
+++ ui/src/main.cpp
@@ -1,44 +1,48 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <ftxui/component/component.hpp>
+//#include <ftxui/dom/elements.hpp>
+//#include <ftxui/screen/screen.hpp>
+//#include <ftxui/component/component.hpp>
 #include <string>
-#include <vector>
+  //client.server_capabilities.signatureHelpProvider = false
 #include <functional>
 #include <memory>
+#include <string>
+#include <iostream>
 //#include <vector>
-
+//
+#include "ftxui/component/component_base.hpp"
 #include "ftxui/component/component.hpp"
 #include "ftxui/component/screen_interactive.hpp"
 #include "ftxui/dom/elements.hpp"
+#include "ftxui/component/component_options.hpp"
 //#include "ftxui/component/renderer.hpp`"
 
+using namespace ftxui;
 int main() {
 //  std::vector<std::string> options = {
 //    "Option 1",
 //    "Option 2",
 //    "Option 3",
 //  };
-  using namespace ftxui;
   auto screen = ScreenInteractive::TerminalOutput();
   int selected = 0;
   MenuOption option;
-
   option.on_enter = screen.ExitLoopClosure();
   auto menu = Container::Vertical(
     {
+      Renderer([]{return separator();}),
       MenuEntry("Option 1"),
       MenuEntry ("Option 2"),
       MenuEntry("Option 3"),
+      Renderer([]{return separator();}),
     },
     &selected);
-  )
+  
   auto renderer = Renderer(menu, [&]{
     return vbox({
-      hbox(text("Selected ="),
-           text(std::to_string(selected))),
-        seperator(),
-      menu->Render() | frame | size(HEGIHT, LESS_THAN, 10),
-    })
+      hbox(text("Selected ="), text(std::to_string(selected))),
+        separator(),
+      menu->Render() | frame | size(HEIGHT, LESS_THAN, 10),
+    }) | border;
   });
   screen.Loop(renderer);
 }
